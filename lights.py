ade58adb8e9b67e49b71d87b28f3619865c8924f
Collin <collin@much.net>
2018-02-09 13:59:23 -0800

    Fancy new title

--- lights.py
+++ lights.py
@@ -1,13 +1,11 @@
-import time,requests,json
+import time,sys,requests,json
 
 data=json.load(open('lightsdata.json'))
 
 token1=data['tokens']['1']
 ip1='http://%s' %data['ips']['1']
-
 token2=data['tokens']['2']
 ip2='http://%s' %data['ips']['2']
-
 name1=data['names']['1']
 name2=data['names']['2']
 
@@ -17,27 +15,43 @@ token=''
 on='{"on":true}'
 off='{"on":false}'
 
+BLUE, RED, WHITE, YELLOW, MAGENTA, GREEN, END = '\033[36;1m', '\033[91;1m', '\33[37;1m', '\33[93;1m', '\033[35;1m', '\033[32;1m', '\033[0m'
+
+sys.stdout.write("\n\n\n"+BLUE+WHITE+
+                 " -------------------------------------------\n"+
+                 WHITE+"| "+MAGENTA+"█░░█ "+BLUE+"█░░█ "+GREEN+"█▀▀ "+MAGENTA+"█░░ "+BLUE+"░▀░ "+GREEN+"█▀▀▀ "+MAGENTA+"█░░█ "+BLUE+"▀▀█▀▀ "+GREEN+"█▀▀"+WHITE+" |\n"+
+                 WHITE+"| "+MAGENTA+"█▀▀█ "+BLUE+"█░░█ "+GREEN+"█▀▀ "+MAGENTA+"█░░ "+BLUE+"▀█▀ "+GREEN+"█░▀█ "+MAGENTA+"█▀▀█ "+BLUE+"░░█░░ "+GREEN+"▀▀█"+WHITE+" |\n"+ 
+                 WHITE+"| "+MAGENTA+"▀░░▀ "+BLUE+"░▀▀▀ "+GREEN+"▀▀▀ "+MAGENTA+"▀▀▀ "+BLUE+"▀▀▀ "+GREEN+"▀▀▀▀ "+MAGENTA+"▀░░▀ "+BLUE+"░░▀░░ "+GREEN+"▀▀▀"+WHITE+" |  "+
+                 WHITE+"v1.0\n"+
+                 " -------------------------------------------\n"+
+                 RED+"Made by: {}Collin Murch ({}@collinmurch{}){}".format(YELLOW, BLUE, YELLOW, END).center(80)+
+                 "\n\n\n".center(0))
+
 def flick(ip, token, light, state):
     r=requests.put('%s/api/%s/lights/%d/state' %(ip, token, light), data=state)
     print(r.text)
 
-choice=input("%s or %s: " %(name1, name2))
+def cycle():
+    for x in lights:
+        flick(ip, token, int(x), on)
+        time.sleep(1)
+        flick(ip, token, int(x), off)
+        time.sleep(1)
 
-if(choice[0:1]==name1[0:1]):
+choice=input("%s or %s: " %(name1, name2)).lower()
+
+if(choice[0:1]==name1[0:1].lower()):
     ip=ip1
     token=token1
-elif(choice[0:1]==name2[0:1]):
+    chosen=true
+elif(choice[0:1]==name2[0:1].lower()):
     ip=ip2
     token=token2
+    chosen=true
 else: 
     print("invalid name.")
-    exit()
 
 r=requests.get('%s/api/%s/lights/' %(ip, token))
 lights=json.loads(r.text).keys()
 
-for x in lights:
-    flick(ip, token, int(x), on)
-    time.sleep(1)
-    flick(ip, token, int(x), off)
-    time.sleep(1)
+cycle()
